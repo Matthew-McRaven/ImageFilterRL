2b2ef680dd4d69040d073fa492bc1819ebf465b7
Matthew McRaven <mkm302@georgetown.edu>
2020-12-15 09:12:51 -0800

    Addiitonal comments to main script.

--- tools/run.py
+++ tools/run.py
@@ -17,6 +17,8 @@ import imgfiltrl.env
 import imgfiltrl.replay
 import imgfiltrl.nn
 
+# Log replay information to harddisk
+# This will enable us to generate graphs in the future.
 class DirLogger:
     def __init__(self, log_dir):
         pass
@@ -55,25 +57,8 @@ def mnist_dataset():
             self.dims = (1, 28, 28)
     return helper()
 
-def cifar10_dataset():
-    import torchvision.datasets, torchvision.transforms
-    from librl.utils import load_split_data
-    class helper:
-        def __init__(self):
-            mean, stdev = .5, .25
-            transformation = torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((mean, mean, mean), (stdev, stdev, stdev))]) 
-
-            # Construct dataloaders from datasets
-            train_dset = torchvision.datasets.CIFAR10("__pycache__/CIFAR10", transform=transformation, download=True)
-            validation_dset = torchvision.datasets.CIFAR10("__pycache__/CIFAR10", transform=transformation, train=False)
-            self.t_loaders, self.v_loaders = load_split_data(train_dset, 100, 3), load_split_data(validation_dset, 1000, 1)
-            self.dims = (3, 32, 32)
-    return helper()
-
 ######################
-# Execute train loop #
+# Neural Net Helpers #
 ######################
 def vpg_helper(hypers, _, policy_net):
     agent = librl.agent.pg.REINFORCEAgent(policy_net, explore_bonus_fn=librl.reward.basic_entropy_bonus())
@@ -87,12 +72,7 @@ def pgb_helper(hypers, critic_net, policy_net):
     agent.train()
     return agent
 
-def ppo_helper(hypers, critic_net, policy_net):
-    actor_loss = librl.nn.pg_loss.PPO(critic_net, explore_bonus_fn=librl.reward.basic_entropy_bonus())
-    agent = librl.agent.pg.ActorCriticAgent(critic_net, policy_net, actor_loss=actor_loss)
-    agent.train()
-    return agent
-
+# Create the inner classifier network.
 def gen_classifier(dims, labels):
     layers = [
         librl.nn.core.cnn.conv_def(4, 4, 1, 2, 1, False),
@@ -117,11 +97,12 @@ def main(args):
     hypers = {}
     hypers['device'] = 'cpu'
     hypers['epochs'] = args.epochs
-    hypers['task_count'] = args.task_count
-    hypers['adapt_steps'] = args.adapt_steps
+    # 
+    hypers['task_count'] = 1
+    # Number of actions that will be suggested each epoch
     hypers['episode_length'] = args.episode_length
-    hypers['max_mlp_layers'] = 10
-    hypers['max_cnn_layers'] = 10
+    # Number of epochs classifiers will be trained for each timestep
+    hypers['adapt_steps'] = args.adapt_steps
     dset = mnist_dataset()
     labels, dims = 10,dset.dims
     network0, network1 = gen_classifier(dims, labels), gen_classifier(dims, labels)
@@ -129,35 +110,34 @@ def main(args):
         torch.nn.CrossEntropyLoss(), train_dataset=dset.train_dset, 
         validation_dataset=dset.validation_dset, normalize_fn=dset.normalize_fn,
         adapt_steps=args.adapt_steps
-    
     )
 
     critic_kernel= librl.nn.core.MLPKernel(4*3, [100, 100])
     critic = librl.nn.critic.ValueCritic(critic_kernel)
     actor_kernel= librl.nn.core.RecurrentKernel(4*3, 100, 3)
-    #actor_kernel= librl.nn.core.MLPKernel(4*3, [100, 100])
     actor = imgfiltrl.actor.FilterTreeActor(actor_kernel, env.observation_space)
     agent = args.alg(hypers, critic, actor)
     agent.train()
 
+    # Create an environment in which to train our agent.
+    # This environment is what will apply filters and train our classifier
     dist = librl.task.TaskDistribution()
     dist.add_task(librl.task.Task.Definition(librl.task.ContinuousGymTask, env=env, agent=agent, episode_length=args.episode_length, 
         replay_ctor=imgfiltrl.replay.ProductEpisodeWithExtraLogs))
+    # Kick off training.
     librl.train.train_loop.cc_episodic_trainer(hypers, dist, librl.train.cc.policy_gradient_step, log_fn=DirLogger(args.log_dir))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train generator network on a task for multiple epochs and record results.")
-    parser.add_argument("--log-dir", dest="log_dir", help="Directory in which to store logs.")
+    parser = argparse.ArgumentParser(description="Train agent to apply effective filters to MNIST photos for image classification.")
+    parser.add_argument("--log-dir", dest="log_dir", help="Directory in which to store logs. Must not already exist")
     # Choose RL algorthim.
     learn_alg_group = parser.add_mutually_exclusive_group()
     learn_alg_group.add_argument("--vpg", action='store_const', const=vpg_helper, dest='alg', help="Train agent using VPG.")
     learn_alg_group.add_argument("--pgb", action='store_const', const=pgb_helper, dest='alg', help="Train agent using PGB.")
-    learn_alg_group.add_argument("--ppo", action='store_const', const=ppo_helper, dest='alg', help="Train agent using PPO.")
     learn_alg_group.set_defaults(alg=vpg_helper)
     # Task distribution hyperparams.
-    parser.add_argument("--epochs", default=10, type=int, help="Number of epochs for which to train the generator network.")
-    parser.add_argument("--episode-length", dest="episode_length", default=200, type=int, help="Number of timestps per episode.")
-    parser.add_argument("--adapt-steps", dest="adapt_steps", default=5, type=int, help="Number of epochs which to the generated network.")
-    parser.add_argument("--task-count", dest="task_count", default=1, type=int, help="Number of times of trials of the generator per epoch.")
+    parser.add_argument("--epochs", default=10, type=int, help="Number of epochs for which to train the filter generating network.")
+    parser.add_argument("--episode-length", dest="episode_length", default=2, type=int, help="Number of timestps per episode.")
+    parser.add_argument("--adapt-steps", dest="adapt_steps", default=2, type=int, help="Number of epochs for which to the classifier networks.")
     args = parser.parse_args()
     main(args)
